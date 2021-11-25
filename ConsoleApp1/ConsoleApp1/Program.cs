872be38d604c2aabeeee7c779851963f030bbe85
Alex Nemes <alexandru.nemes@ppb.com>
2021-11-25 21:05:03 +0200

    working with strings

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Text;
 
 namespace ConsoleApp1
 {
@@ -7,6 +9,51 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! Test");
+
+            string myString = "Hello";
+            string myNextString = "World";
+
+            string a = "a";
+            string b = "b";
+            string c = "c";
+            string random = "B";
+
+            int resultLeft = string.Compare(a, b);
+            int resultRight = string.Compare(c, b);
+            int resultEqual = string.Compare(random, b, true);
+
+            //Concat
+
+            myString = myString + " world";
+            string helloWorld = myString + " " + myNextString;
+
+            Stopwatch stopWatch = Stopwatch.StartNew();
+
+            stopWatch.Start();
+
+            for (int i = 0; i < 100000; i++)
+            {
+
+                myString += i;
+
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.Elapsed);
+
+            Stopwatch stopWatch1 = Stopwatch.StartNew();
+            stopWatch1.Start();
+
+            StringBuilder builder = new StringBuilder();
+            builder.Append(myString);
+            for(int i = 0; i < 100000; i++)
+            {
+                builder.Append(i);
+            }
+
+            stopWatch1.Stop();
+            Console.WriteLine("For stringBuilder = " + stopWatch1.Elapsed);
+
         }
     }
 }
